77ed070c2d1c4bfd2a0e2adacdd8784260f391e7
Casper da Costa-Luis <casper.dcl@physics.org>
2020-02-17 15:35:37 +0000

    action: test, lint

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const { execSync } = require("child_process");
 
-function run(cmd, env_vars){
-  return execSync(cmd, { encoding: "utf8", stdio: "inherit", env: env_vars });
+function run(cmd, envVars) {
+	return execSync(cmd, { encoding: "utf8", stdio: "inherit", env: envVars });
 }
 
 function main() {
-  if (process.env.INPUT_SKIP_INSTALL) {
-    console.log("Skipping install");
-  } else {
-    run("go get mvdan.cc/sh/v3/cmd/shfmt", {"GO111MODULE": "on"});
-  }
-  console.log("Running shfmt");
-  run(`shfmt ${process.env.INPUT_SHFMT_ARGS}`);
+	if (process.env.INPUT_SKIP_INSTALL) {
+		console.log("Skipping install");
+	} else {
+		run("go get mvdan.cc/sh/v3/cmd/shfmt", {
+			GO111MODULE: "on",
+			GOCACHE: "/tmp",
+			GOPATH: process.env.HOME,
+		});
+	}
+	console.log("Running shfmt");
+	run(`shfmt ${process.env.INPUT_SHFMT_ARGS}`);
 }
 
 main();
