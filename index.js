97ce49b211286ad4a0a2afea9e0dc6b6e075f1b4
Casper da Costa-Luis <casper.dcl@physics.org>
2020-02-17 17:28:15 +0000

    action: fix install

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 const { execSync } = require("child_process");
 
-function run(cmd, envVars) {
-	return execSync(cmd, { encoding: "utf8", stdio: "inherit", env: envVars });
+function run(cmd, envAppend = {}) {
+	return String(
+		execSync(cmd, {
+			encoding: "utf8",
+			env: Object.assign(process.env, envAppend),
+		}),
+	).trim();
 }
 
 function main() {
 	if (process.env.INPUT_SKIP_INSTALL) {
 		console.log("Skipping install");
 	} else {
-		run("go get mvdan.cc/sh/v3/cmd/shfmt", {
-			GO111MODULE: "on",
-			GOCACHE: "/tmp",
-			GOPATH: process.env.HOME,
-		});
+		run("go get mvdan.cc/sh/v3/cmd/shfmt", { GO111MODULE: "on" });
 	}
 	console.log("Running shfmt");
-	run(`shfmt ${process.env.INPUT_SHFMT_ARGS}`);
+	run(`shfmt ${process.env.INPUT_SHFMT_ARGS}`, {
+		PATH: `${run("go env GOPATH")}/bin:${process.env.PATH}`,
+	});
 }
 
 main();
